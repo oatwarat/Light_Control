74fde9788d708d6fbe1d45f9e693ac221e644e7e
nartnattakrivichian <nartnatta.k@ku.th>
2023-02-08 13:46:11 +0700

    add room

---
+++ frontend/lightcontrol-frontend/src/components/AddRoom.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import '../styles/AddRoom.css'
+
+const AddRoom = () => {
+    const [room, setRoom] = useState("")
+
+    return (
+        <div className='add-room'>
+            
+        </div>
+    )
+}
