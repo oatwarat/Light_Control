3ee2fac9f13e10dfac13d4b1ee0eca9d18cfb206
worakornk <worakornk@gmail.com>
2023-02-08 15:36:48 +0700

    update Card

--- frontend/lightcontrol-frontend/src/components/RoomCard.jsx
+++ frontend/lightcontrol-frontend/src/components/RoomCard.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 // import { Link } from 'react-router-dom'
 import '../styles/RoomCard.css'
 // import defaultIcon from '../assets/default.png' // Default Icon
 
-const Card = ({roomName,Icon,Status,Brightness}) => {
+const Card = ({roomName,Status="Mannual",Brightness=0}) => {
+
+	// const []
+
 	const getCardColorClass = ({Status,Brightness}) => {
         if ((Status == "Mannual" || Status == "Auto") && Brightness == 0) {
             return "OffCardColor"
@@ -13,18 +16,48 @@ const Card = ({roomName,Icon,Status,Brightness}) => {
             return "AutoCardColor"
         }
 	}
-		
+
+	const StatusSwtich = (Status) => {
+		if (Status == "Mannual") {
+			Status = "Auto"
+		} else if (Status == "Auto") {
+			Status = "MannualPhys"
+		} else if (Status == "MannualPhys"){
+			Status = "MannualWeb"
+		} else if (Status == "MannualWeb"){
+			Status = "Mannual"
+		}
+	}
+
+	const BrightnessValue = useState("")
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		const ressult = {
+			roomName,
+			Status,
+			Brightness : BrightnessValue
+		}
+		const data = ressult.json()
+		fetch('http://localhost:5000/api/room',
+		{
+			method: 'POST',
+			body: data
+		}
+		).then((res) => console.log("Hi"))
+
+	}
+
 	// const routeName = `/${category}/${id}`
 
 	return (
 		<div>
-			<p>Hello World</p>
 			{/* <div className={`${getCardColorClass(Status,Brightness)}`}> */}
-			<div className="OffCardColor">
-      			<h4><b>Living Room</b><button className = "Status">Status</button></h4>
-
+			<div>
+      			<h4><b>roomName</b><button className = {Status} onClick={StatusSwtich}>Status</button></h4>
+                <input onChange = {e => BrightnessValue}/>
+				<button className = "Brightness" onClick={handleSubmit}>change level</button>
 			</div>
-    		
 		</div>
 	)
 }
