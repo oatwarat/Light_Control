2c30de5d41543a3427bc790be944511256850abc
nartnattakrivichian <nartnatta.k@ku.th>
2023-02-08 11:57:14 +0700

    start home page

---
+++ frontend/lightcontrol-frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react'
+import '../styles/Home.css'
+
+const Home = () => {
+
+    return (
+        <h1>Welcome Home</h1>
+    )
+
+}
+
+export default Home
