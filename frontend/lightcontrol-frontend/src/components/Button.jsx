6e15393b8ab5a07302c8442f6c2ea4fb5893cbff
nartnattakrivichian <nartnatta.k@ku.th>
2023-02-08 13:20:17 +0700

    homepage template

---
+++ frontend/lightcontrol-frontend/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Button.css'
+
+const Button = () => {
+    return (
+        <button className='button-login'>
+            Login 
+        </button>
+    )
+}
+
+export default Button
--- frontend/lightcontrol-frontend/src/pages/Home.jsx
+++ frontend/lightcontrol-frontend/src/pages/Home.jsx
@@ -1,10 +1,27 @@
 import React, { useEffect, useState } from 'react'
+import Button from '../components/Button'
 import '../styles/Home.css'
 
 const Home = () => {
 
     return (
-        <h1>Welcome Home</h1>
+        
+        <div className='container'>
+            <div>
+                <Button />
+            </div>
+            <div className='welcome'>
+                <h1>Welcome Home,</h1>
+                <h2>User's name</h2>
+            </div>
+
+            <div className='card-sample'>
+                <div className='bedroom'></div>
+                <div className='kitchen'></div>
+                <div className='livingroom'></div>
+            </div>
+
+        </div>
     )
 
 }
